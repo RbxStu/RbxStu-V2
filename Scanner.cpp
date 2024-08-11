cfee08b4269714c7988af9e15dfbcaf995932cec
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2024-08-11 18:05:27 -0300

    feat: complete mask + signature parsing. complete Scanner, scanner now uses multiple threads to scan, resulting on increased scanning speeds.

--- Scanner.cpp
+++ Scanner.cpp
@@ -6,9 +6,80 @@
 
 std::shared_ptr<Scanner> Scanner::pInstance;
 
+std::vector<void *> Scanner::ScanInternal(const unsigned char *buffer, const std::size_t bufferSize,
+                                          const Signature &signature,
+                                          const MEMORY_BASIC_INFORMATION &memoryInformation) {
+    auto signatureLength = signature.size();
+    std::vector<void *> vec{};
+    for (std::uint32_t i = 0; i < bufferSize - signatureLength; i++) {
+        bool success = true;
+        auto offsetBuffer = reinterpret_cast<const char *>(reinterpret_cast<std::uintptr_t>(buffer) + i);
+
+        for (std::uint32_t j = 0; j < signatureLength; j++) {
+            if (!signature[j].bIsWildcard &&
+                signature[j].szlookForByte !=
+                        *reinterpret_cast<const unsigned char *>(reinterpret_cast<std::uintptr_t>(offsetBuffer) + j)) {
+                success = false; // Byte is not the same.
+                break;
+            }
+        }
+        if (success) {
+            vec.push_back(
+                    reinterpret_cast<void *>(reinterpret_cast<std::uintptr_t>(memoryInformation.BaseAddress) + i));
+        }
+    }
+    Sleep(1);
+    return vec;
+}
 std::shared_ptr<Scanner> Scanner::GetSingleton() {
     if (Scanner::pInstance == nullptr)
         Scanner::pInstance = std::make_shared<Scanner>();
 
     return Scanner::pInstance;
+}
+std::vector<void *> Scanner::Scan(const Signature &signature, const void *lpStartAddress) {
+    const auto logger = Logger::GetSingleton();
+    std::vector<std::future<std::vector<void *>>> scansVector{};
+    std::vector<void *> results{};
+    MEMORY_BASIC_INFORMATION memoryInfo{};
+    logger->PrintInformation(RbxStu::ByteScanner,
+                             std::format("Beginning scan from address {} to far beyond!", lpStartAddress));
+
+    auto startAddress = reinterpret_cast<std::uintptr_t>(lpStartAddress);
+
+    while (VirtualQuery(reinterpret_cast<void *>(startAddress), &memoryInfo, sizeof(MEMORY_BASIC_INFORMATION))) {
+        scansVector.push_back(std::async(std::launch::async, [memoryInfo, &logger, &signature]() {
+            bool valid = memoryInfo.State == MEM_COMMIT;
+            valid &= (memoryInfo.Protect & PAGE_GUARD) == 0;
+            valid &= (memoryInfo.Protect & PAGE_NOACCESS) == 0;
+            valid &= memoryInfo.Type == MEM_PRIVATE || memoryInfo.Type == MEM_IMAGE;
+
+            if (!valid) {
+                // logger->PrintInformation(
+                //         RbxStu::ByteScanner,
+                //         std::format("Memory block at address {} is invalid for scanning.",
+                //         memoryInfo.BaseAddress));
+                return std::vector<void *>{};
+            }
+            auto *buffer = new unsigned char[memoryInfo.RegionSize];
+            memcpy(buffer, memoryInfo.BaseAddress, memoryInfo.RegionSize);
+
+            auto scanResult = Scanner::ScanInternal(buffer, memoryInfo.RegionSize, signature, memoryInfo);
+            delete[] buffer;
+            return scanResult;
+        }));
+
+        startAddress += memoryInfo.RegionSize;
+    }
+
+    for (int i = 0; i < scansVector.size(); ++i) {
+        auto async_result = scansVector[i].get();
+        for (const auto &e: async_result) {
+            results.push_back(e);
+        }
+    }
+
+    logger->PrintInformation(RbxStu::ByteScanner,
+                             std::format("Scan finalized. Found {} candidates for signature.", results.size()));
+    return results;
 }
--- Scanner.hpp
+++ Scanner.hpp
@@ -2,6 +2,8 @@
 // Created by Dottik on 10/8/2024.
 //
 #pragma once
+#include <Windows.h>
+#include <future>
 #include <memory>
 #include <string>
 #include <vector>
@@ -16,11 +18,48 @@ public:
     unsigned char szlookForByte;
     bool bIsWildcard;
 
-    static Signature GetSignatureFromString(const std::string &aob, const std::string &mask) {
-        // TODO: Implement signature parser.
+    static Signature GetSignatureFromString(_In_ const std::string &aob, _In_ const std::string &mask) {
+        auto logger = Logger::GetSingleton();
+        auto nAob = Utilities::SplitBy(aob, ' ');
+        auto nMask = Utilities::SplitBy(mask, ' ');
+        if (nAob.size() != nMask.size()) {
+            logger->PrintError(RbxStu::ByteScanner,
+                               std::format("Failed to parse signature + mask. Reason: The array of bytes and the mask "
+                                           "do not match in size when normalized. Size of AOB: {}. Size of Mask: {}",
+                                           nAob.size(), nMask.size()));
+            return {};
+        }
+
+        for (std::int32_t i = 0; i < nMask.size(); i++) {
+            for (std::int32_t j = 0; j < nMask[i].size(); i++) {
+                nMask[i][j] = std::tolower(nMask[i][j]); // Normalize mask to lower characters to avoid issues.
+            }
+        }
+
+        Signature sig;
+        for (std::int32_t i = 0; i < nAob.size(); i++) {
+            if (nMask[i].find('x') != std::string::npos) {
+                sig.push_back(SignatureByte{static_cast<unsigned char>('\0'), true});
+                continue;
+            }
+
+            auto parsed = strtoul(nAob[i].data(), nullptr, 16);
+
+            if (parsed > 255) { // We needn't bottom check, as its an unsigned long.
+                logger->PrintWarning(
+                        RbxStu::ByteScanner,
+                        std::format(
+                                "Failed to parse signature + mask. Reason: Value outside of the unsigned "
+                                "char range. Value parsed: {}; Skipping byte, this may result in undefined behaviour.",
+                                parsed));
+                continue;
+            }
+
+            sig.push_back(SignatureByte{static_cast<unsigned char>(parsed), false});
+        }
     }
 
-    static Signature GetSignatureFromIDAString(const std::string &aob) {
+    static Signature GetSignatureFromIDAString(_In_ const std::string &aob) {
         auto logger = Logger::GetSingleton();
         Signature sig;
 
@@ -33,10 +72,12 @@ public:
             auto parsed = strtoul(byte.data(), nullptr, 16);
 
             if (parsed > 255) { // We needn't bottom check, as its an unsigned long.
-                logger->PrintWarning(RbxStu::ByteScanner,
-                                     std::format("Failed to parse IDA signature. Reason: Value outside of the unsigned "
-                                                 "char range. Value parsed: {}; Skipping byte, this may result in undefined behaviour.",
-                                                 parsed));
+                logger->PrintWarning(
+                        RbxStu::ByteScanner,
+                        std::format(
+                                "Failed to parse IDA signature. Reason: Value outside of the unsigned "
+                                "char range. Value parsed: {}; Skipping byte, this may result in undefined behaviour.",
+                                parsed));
                 continue;
             }
 
@@ -47,14 +88,29 @@ public:
     }
 };
 
-
+/// @brief Allows you to do AOB Scans on the current process with a signature.
 class Scanner final {
     static std::shared_ptr<Scanner> pInstance;
+
+    /// @brief Matches the buffer contents against the signature.
+    /// @param buffer [in] The buffer containing the chunk of memory to search at
+    /// @param bufferSize [in] The size of the buffer.
+    /// @param signature [in] Vector containing the SignatureBytes representing the signature in memory.
+    /// @param memoryInformation [in] The basic memory information from which this information was grabbed. Required to
+    /// calculate the base address of the returned void pointers.
+    /// @return A vector containing all the addresses that matched, translated from the buffers address into the
+    /// BaseAddress provided by memoryInformation.
+    static std::vector<void *> ScanInternal(_In_ const unsigned char *buffer, _In_ const std::size_t bufferSize,
+                                            _In_ const Signature &signature,
+                                            _In_ const MEMORY_BASIC_INFORMATION &memoryInformation);
 
 public:
     /// @brief Obtains the Singleton for the Scanner instance.
     /// @return Returns a shared pointer to the global Scanner singleton instance.
     static std::shared_ptr<Scanner> GetSingleton();
 
-    std::vector<void *> Scan(const Signature &signature, void *lpStartAddress) {}
+    /// @brief Scans from the given start address for the given signature.
+    /// @param signature [in] A Vector containing the SignatureByte list that must be matched.
+    /// @param lpStartAddress [in, opt] The address to start scanning from.
+    std::vector<void *> Scan(_In_ const Signature &signature, _In_opt_ const void *lpStartAddress);
 };
