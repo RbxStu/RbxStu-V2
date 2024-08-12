558333043377cc3d49351c9ce7010d8c7e609895
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2024-08-12 20:00:51 -0300

    feat: add exception handler for the purposes of debugging; will be moved into its own segment further on.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,112 @@
 //
 // Created by Dottik on 10/8/2024.
 //
+#include <Windows.h>
 
 #include <Logger.hpp>
-#include <Windows.h>
 #include <cstdio>
+#include <iostream>
+
+#include <DbgHelp.h> // Must be positioned here because else include failure.
 
 #include "RobloxManager.hpp"
 #include "Scanner.hpp"
 
+long exception_filter(PEXCEPTION_POINTERS pExceptionPointers) {
+    const auto *pContext = pExceptionPointers->ContextRecord;
+    printf("\r\n-- WARNING: Exception handler caught an exception\r\n");
+
+    if (pExceptionPointers->ExceptionRecord->ExceptionCode == EXCEPTION_ACCESS_VIOLATION) {
+        printf("Exception Identified: EXCEPTION_ACCESS_VIOLATION\r\n");
+    }
+
+    printf("Exception Caught         @ %p\r\n", pExceptionPointers->ContextRecord->Rip);
+    printf("Module.dll               @ %p\r\n", reinterpret_cast<std::uintptr_t>(GetModuleHandleA("Module.dll")));
+    printf("Rebased Module           @ 0x%p\r\n",
+           pExceptionPointers->ContextRecord->Rip - reinterpret_cast<std::uintptr_t>(GetModuleHandleA("Module.dll")));
+    printf("RobloxStudioBeta.exe     @ %p\r\n",
+           reinterpret_cast<std::uintptr_t>(GetModuleHandleA("RobloxStudioBeta.exe")));
+
+    printf("Rebased Studio           @ 0x%p\r\n",
+           pContext->Rip - reinterpret_cast<std::uintptr_t>(GetModuleHandleA("RobloxStudioBeta.exe")));
+
+    printf("-- START REGISTERS STATE --\r\n\r\n");
+
+    printf("-- START GP REGISTERS --\r\n");
+
+    printf("RAX: 0x%p\r\n", pContext->Rax);
+    printf("RBX: 0x%p\r\n", pContext->Rbx);
+    printf("RCX: 0x%p\r\n", pContext->Rcx);
+    printf("RDX: 0x%p\r\n", pContext->Rdx);
+    printf("RDI: 0x%p\r\n", pContext->Rdi);
+    printf("RSI: 0x%p\r\n", pContext->Rsi);
+    printf("-- R8 - R15 --\r\n");
+    printf("R08: 0x%p\r\n", pContext->R8);
+    printf("R09: 0x%p\r\n", pContext->R9);
+    printf("R10: 0x%p\r\n", pContext->R10);
+    printf("R11: 0x%p\r\n", pContext->R11);
+    printf("R12: 0x%p\r\n", pContext->R12);
+    printf("R13: 0x%p\r\n", pContext->R13);
+    printf("R14: 0x%p\r\n", pContext->R14);
+    printf("R15: 0x%p\r\n", pContext->R15);
+    printf("-- END GP REGISTERS --\r\n\r\n");
+
+    printf("-- START STACK POINTERS --\r\n");
+    printf("RBP: 0x%p\r\n", pContext->Rbp);
+    printf("RSP: 0x%p\r\n", pContext->Rsp);
+    printf("-- END STACK POINTERS --\r\n\r\n");
+
+    printf("-- END REGISTERS STATE --\r\n\r\n");
+
+
+    printf("    -- Stack Trace:\r\n");
+    SymInitialize(GetCurrentProcess(), nullptr, TRUE);
+
+    void *stack[256];
+    const unsigned short frameCount = RtlCaptureStackBackTrace(0, 100, stack, nullptr);
+
+    for (unsigned short i = 0; i < frameCount; ++i) {
+        auto address = reinterpret_cast<DWORD64>(stack[i]);
+        char symbolBuffer[sizeof(SYMBOL_INFO) + MAX_SYM_NAME * sizeof(TCHAR)];
+        auto *symbol = reinterpret_cast<SYMBOL_INFO *>(symbolBuffer);
+        symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
+        symbol->MaxNameLen = MAX_SYM_NAME;
+
+        DWORD value{};
+        DWORD *pValue = &value;
+        if (SymFromAddr(GetCurrentProcess(), address, nullptr, symbol) && ((*pValue = symbol->Address - address)) &&
+            SymFromAddr(GetCurrentProcess(), address, reinterpret_cast<PDWORD64>(pValue), symbol)) {
+            printf(("[Stack Frame %d] Inside %s @ 0x%p; Studio Rebase: 0x%p\r\n"), i, symbol->Name, address,
+                   address - reinterpret_cast<std::uintptr_t>(GetModuleHandleA("RobloxStudioBeta.exe")) + 0x140000000);
+        } else {
+            printf(("[Stack Frame %d] Unknown Subroutine @ 0x%p; Studio Rebase: 0x%p\r\n"), i, symbol->Name, address,
+                   address - reinterpret_cast<std::uintptr_t>(GetModuleHandleA("RobloxStudioBeta.exe")) + 0x140000000);
+        }
+    }
+    std::cout << std::endl;
+    std::stringstream sstream{};
+    for (unsigned short i = 0; i < frameCount; ++i) {
+        sstream << ("0x") << std::hex << reinterpret_cast<std::uintptr_t>(stack[i]);
+        if (i < frameCount) {
+            sstream << (" -> ");
+        } else {
+            sstream << ("\r\n");
+        }
+    }
+
+    std::cout << sstream.str();
+
+    // Clean up
+    SymCleanup(GetCurrentProcess());
+    MessageBoxA(nullptr, ("ERROR"), ("ERROR. LOOK AT CLI."), MB_OK);
+    printf("Stack frames captured - Waiting for 30s before handing to executee... \r\n");
+    Sleep(30000);
+
+    return EXCEPTION_EXECUTE_HANDLER;
+}
+
 int main() {
+    SetUnhandledExceptionFilter(exception_filter);
     AllocConsole();
     const auto logger = Logger::GetSingleton();
     logger->Initialize(true);
@@ -20,6 +117,9 @@ int main() {
     Sleep(1000);
     logger->PrintInformation(RbxStu::MainThread, std::format("Capability for identity 10: {}",
                                                              robloxManager->IdentityToCapability(10).value()));
+
+    robloxManager->GetRobloxPrint()(RBX::Console::MessageType::InformationBlue,
+                                    "Hello, world! I'm not here for %s. I'm here for %s.", "NOTHING", "EVERYTHING");
 
     return 0;
 }
@@ -48,5 +148,5 @@ BOOL WINAPI DllMain(const HINSTANCE hModule, const DWORD fdwReason, const LPVOID
             // Perform any necessary cleanup.
             break;
     }
-    return true; // Successful DLL_PROCESS_ATTACH.
+    return true;
 }
