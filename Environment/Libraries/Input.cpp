e82fa8c12f3f72a345d50b2fa2a8cc63ab6e805a
Pixeluted <pixeluted@proton.me>
2024-08-22 12:12:59 +0200

    ref: Moved all input custom functions to its own file

---
+++ Environment/Libraries/Input.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Pixeluted on 22/08/2024.
+//
+
+#include "Input.hpp"
+#include <Windows.h>
+
+namespace RbxStu {
+    namespace Input {
+        int isrbxactive(lua_State *L) {
+            lua_pushboolean(L, GetForegroundWindow() == GetCurrentProcess());
+            return 1;
+        }
+    } // namespace Input
+} // namespace RbxStu
+
+std::string Input::GetLibraryName() { return "input"; };
+luaL_Reg *Input::GetLibraryFunctions() {
+    auto reg = new luaL_Reg[]{{"isrbxactive", RbxStu::Input::isrbxactive},
+                              {"isgameactive", RbxStu::Input::isrbxactive},
+
+                              {nullptr, nullptr}};
+
+    return reg;
+}
