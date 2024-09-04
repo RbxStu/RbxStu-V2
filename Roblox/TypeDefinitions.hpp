374037b167b1d9cbcf19f7f7638e8a434bd0d372
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2024-09-04 20:00:14 -0300

    feat: fix structs.

--- Roblox/TypeDefinitions.hpp
+++ Roblox/TypeDefinitions.hpp
@@ -84,149 +84,86 @@ namespace RBX {
 
     struct DataModel {
         void *vftable;
-        char _8[1];
-        char _9[3];
-        char _c[1];
-        char _d[3];
-        char _10[4];
-        char _14[4];
-        char _18[2];
-        char _1a[1];
-        char _1b[4];
-        char _1f[1];
-        char _20[4];
-        char _24[4];
+        char _8[0x28];
         std::string m_szstdstringJobId;
-        char _48[2];
-        char _4a[6];
-        char _50[6];
-        char _56[2];
-        std::uint64_t m_qwCreatorId;
-        std::uint64_t m_qwGameId;
-        std::uint64_t m_qwPlaceId;
-        char _70[0x10];
-        std::uint32_t m_dwPlaceVersion;
-        char _88[0x20];
-        char _a8[0x10];
-        char _b8[8];
-        char _c0[8];
-        char _c8[0x38];
-        char _100[8];
-        char _108[0x38];
-        char _140[0x30];
-        char _170[0x10];
+        char _50[0x10];
+        uint64_t m_qwCreatorId;
+        uint64_t m_qwGameId;
+        uint64_t m_qwPlaceId;
+        char _78[8];
+        char _80[8];
+        uint32_t m_dwPlaceVersion;
+        char _90[0x30];
+        char _c0[0x40];
+        char _100[0x40];
+        char _140[0x40];
         char _180[0x10];
-        void *m_pScriptContext;
-        char _198[8];
+        char _190[8];
+         void *m_pScriptContext;
         char _1a0[0x20];
-        char _1c0[0x20];
-        char _1e0[0x1c];
-        char _1fc[4];
-        char _200[8];
-        char _208[0x2c];
-        char _234[4];
-        char _238[4];
-        char _23c[4];
-        char _240[0x10];
-        char _250[0x20];
-        char _270[0x10];
-        char _280[0x10];
-        char _290[8];
-        std::uint64_t m_qwPrivateServerId;
-        char _2a0[0x10];
-        char _2b0[0xd];
-        char _2bd[3];
-        char _2c0[0x18];
-        char _2d8[0x20];
-        std::int64_t m_dwPlaceId;
-        std::uint64_t m_qwPrivateServerOwnerId;
-        char _308[0x38];
-        char _340[0x30];
-        char _370[8];
-        char _378[8];
-        char _380[4];
-        char _384[2];
-        char _386[0xa];
-        char _390[8];
-        char _398[8];
-        char _3a0[8];
-        char _3a8[0x18];
-        char _3c0[0x30];
-        char _3f0[8];
-        char _3f8[8];
+        char _1c0[0x40];
+        char _200[0x40];
+        char _240[0x38];
+        char _278[8];
+        char _280[0x18];
+        char _298[8];
+        uint64_t m_qwPrivateServerId;
+        char _2a8[0x18];
+        char _2c0[0x20];
+        char _2e0[0x20];
+        int64_t m_dwPlaceId;
+        uint64_t m_qwPrivateServerOwnerId;
+        char _310[0x30];
+        char _340[0x40];
+        char _380[8];
+        char _388[0x38];
+        char _3c0[0x40];
+        char _400[8];
         bool m_bIsParallelPhase;
-        char _404[4];
-        char _408[8];
-        char _410[0x30];
-        char _440[0x20];
+        char _40c[0x34];
+        char _440[0x28];
         enum RBX::DataModelType m_dwDataModelType;
-        char _464[4];
-        char _468[0x18];
-        char _480[0x3c];
-        char _4bc[4];
+        char _46c[0x14];
+        char _480[0x40];
         char _4c0[0x40];
         char _500[0x40];
-        char _540[1];
-        char _541[1];
-        char _542[1];
+        char _540[9];
+        char _549[1];
+        char _54a[1];
         bool m_bIsClosed;
         bool m_bIsContentLoaded;
-        char _543[0x3b];
+        char _54d[0x33];
         char _580[0x40];
-        char _5c0[0x20];
-        char _5e0[0x10];
+        char _5c0[0x28];
+        char _5e8[0x10];
         bool m_bIsLoaded;
-        char _5f1[2];
+        char _5f9[2];
         bool m_bIsUniverseMetadataLoaded;
-        char _5f4[0xc];
-        char _600[0x10];
-        char _610[0x30];
-        char _640[0x28];
-        char _668[8];
-        char _670[8];
-        char _678[8];
-        char _680[8];
-        char _688[8];
-        char _690[8];
-        char _698[0x28];
+        char _5fc[4];
+        char _600[0x40];
+        char _640[0x40];
+        char _680[0x40];
         char _6c0[0x40];
-        char _700[0x31];
-        char _731[0xf];
-        char _740[8];
-        char _748[4];
-        char _74c[4];
-        char _750[4];
-        char _754[4];
-        char _758[0x20];
-        char _778[8];
-        char _780[0x38];
-        char _7b8[2];
-        char _7ba[1];
-        char _7bb[1];
-        char _7bc[1];
-        char _7bd[3];
+        char _700[0x40];
+        char _740[0x40];
+        char _780[0x40];
         char _7c0[0x40];
         char _800[0x40];
         char _840[0x40];
         char _880[0x40];
         char _8c0[0x40];
-        char _900[8];
-        char _908[0x18];
-        char _920[8];
-        char _928[8];
-        char _930[0xc];
-        char _93c[1];
+        char _900[0x28];
+        char _928[0x18];
+        char _940[5];
         bool m_bIsBeingClosed;
-        std::int64_t m_qwGameLaunchIntent;
-        char _948[0x18];
-        char _960[4];
-        char _964[1];
-        char _965[0xb];
+        int64_t m_qwGameLaunchIntent;
+        char _950[0x28];
     };
 
 
     struct ModuleScript {
         char _0[8];
+        char _1[8];
         char _8[0x38];
         char _40[0x40];
         char _80[0x40];
@@ -242,6 +179,7 @@ namespace RBX {
 
     template<typename T>
     class PointerEncryption final {
+    public:
         std::uintptr_t addressOne;
         std::uintptr_t *addressTwo;
 
@@ -255,6 +193,29 @@ namespace RBX {
                     return addressOne - *addressTwo;
                 default:
                     return *addressTwo - addressOne;
+            }
+        }
+    };
+
+    template<typename T>
+    class PointerOffsetEncryption final {
+    public:
+        std::uintptr_t address = 0;
+        std::uintptr_t offset = 0;
+
+        PointerOffsetEncryption(void *address, std::uintptr_t offset) {
+            this->address = reinterpret_cast<std::uintptr_t>(address);
+            this->offset = offset;
+        }
+
+        T *DecodePointerWithOffsetEncryption(const PointerEncryptionType pointerEncryption) {
+            switch (pointerEncryption) {
+                case ADD:
+                    return reinterpret_cast<T *>(address + offset);
+                case SUB:
+                    return reinterpret_cast<T *>(address - offset);
+                default:
+                    return nullptr;
             }
         }
     };
