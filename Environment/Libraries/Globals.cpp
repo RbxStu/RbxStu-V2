4149519ae0cc1d9ee516c34a5db0e48da84f0bfa
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2024-09-06 20:48:53 -0300

    chore: removed unnecessary bloat.

--- Environment/Libraries/Globals.cpp
+++ Environment/Libraries/Globals.cpp
@@ -26,37 +26,15 @@ namespace RbxStu {
         return 1;
     }
 
-
-    int require(lua_State *L) {
-        Utilities::checkInstance(L, 1, "ModuleScript");
-
-        auto moduleScript = *static_cast<RBX::ModuleScript **>(lua_touserdata(L, 1));
-        moduleScript->m_bIsRobloxScriptModule = true;
-        auto robloxState = Scheduler::GetSingleton()->GetGlobalRobloxState().value();
-        lua_getglobal(robloxState, "require");
-        lua_xmove(robloxState, L, 1);
-        lua_pushvalue(L, 1);
-        auto status = lua_pcall(L, 1, 1, 0);
-        moduleScript->m_bIsRobloxScriptModule = false;
-
-        if (status != lua_Status::LUA_OK)
-            lua_error(L);
-
-        return 1;
-    }
-
 } // namespace RbxStu
 
 
 std::string Globals::GetLibraryName() { return "rbxstu"; }
 luaL_Reg *Globals::GetLibraryFunctions() {
     // WARNING: you MUST add nullptr at the end of luaL_Reg declarations, else, Luau will choke.
-    const auto reg = new luaL_Reg[]{
-            {"isluau", RbxStu::isluau},
-            //{"require", RbxStu::require}, Disabled for the require made in init script, DO NOT RE-ENABLE
-            // {"decompile", RbxStu::decompile}, // Stripped for the reasons of security.
-
-            {nullptr, nullptr}};
+    const auto reg = new luaL_Reg[]{{"isluau", RbxStu::isluau},
+                                    // {"decompile", RbxStu::decompile}, // Stripped for the reasons of security.
+                                    {nullptr, nullptr}};
 
     return reg;
 }
