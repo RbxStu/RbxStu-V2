6d3a55c385eb253e5cb22ae7f11c6af5334ddc7b
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2024-10-12 10:17:09 -0300

    fix: remove useless include

--- Disassembler/Disassembler.hpp
+++ Disassembler/Disassembler.hpp
@@ -2,8 +2,6 @@
 // Created by Dottik on 26/8/2024.
 //
 #pragma once
-#include <complex.h>
-
 #include <map>
 #include <memory>
 #include <optional>
@@ -26,5 +24,5 @@ public:
     std::optional<std::unique_ptr<DisassembledChunk>> GetInstructions(_In_ DisassemblyRequest &disassemblyRequest);
     std::optional<void *> TranslateRelativeLeaIntoRuntimeAddress(const cs_insn &insn);
     void *ObtainPossibleStartOfFunction(void *mapped);
-    void * ObtainPossibleEndFromStart(void * mapped);
+    void *ObtainPossibleEndFromStart(void *mapped);
 };
